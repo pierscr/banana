c5e0171c22ac68b2cd754adf1d95909fc13f1090
pierscr <pierscr@libero.it>
2018-09-06 16:27:13 +0200

    parametrics tips added fot multibar chart

--- src/app/panels/multibar/module.js
+++ src/app/panels/multibar/module.js
@@ -58,8 +58,9 @@ function (angular, app, _, $, d3, d3tip,palette) {
       },
       field1: '',
       field2: '',
-      labelXaxis: '',
-      labelYaxis: '',
+      labelXaxis: 'category1',
+      labelYaxis: 'category2',
+      docname:'documents',
       max_number_r: 10,
       max_rows: 10,
       spyable: true,
@@ -331,7 +332,7 @@ function (angular, app, _, $, d3, d3tip,palette) {
                .attr("fill", "#000")
                .attr("font-weight", "bold")
                .attr("text-anchor", "start")
-               .text(scope.panel.labelYaxis);
+               .text(scope.panel.labelXaxis+" - "+scope.panel.labelYaxis);
 
           var zoom=d3.extensions.zoomBounded()
                               .scaleExtent([1, width])
@@ -351,19 +352,19 @@ function (angular, app, _, $, d3, d3tip,palette) {
               .html(function(p) {
                   var patent_number;
                   if(scope.panel.number_mode==="F"){
-                    patent_number="<div><strong>"+scope.panel.labelYaxis+"</strong> <span style='color:red'>" + p.count + "</span></div>";
+                    patent_number="<div><strong>"+scope.panel.docname+"</strong> <span style='color:red'>" + p.count + "</span></div>";
                   }else{
-                    patent_number="<div><strong>"+scope.panel.labelYaxis+"percentage</strong> <span style='color:red'>" + ((p.count/scope.data.field1stat.tot_docs)*100).toFixed(2)  + "%</span></div>";
+                    patent_number="<div><strong>"+scope.panel.docname+"percentage</strong> <span style='color:red'>" + ((p.count/scope.data.field1stat.tot_docs)*100).toFixed(2)  + "%</span></div>";
                   }
 
-                  return "<div><strong>Cluster name</strong> <span style='color:red'>" + p.val.substr(0,12)+ "</span></div>"+
+                  return "<div><strong>"+scope.panel.labelXaxis+"</strong> <span style='color:red'>" + p.val.substr(0,12)+ "</span></div>"+
                   patent_number+
-                  "<div><strong>Unique patent category</strong> <span style='color:red'>" + this.__data__.top_field2.numBuckets + "</span></div>"+
-                  "<div><strong>"+scope.panel.labelYaxis+" category</strong> <span style='color:red'>" + this.__data__.top_field2.allBuckets.count + "</span></div>"+
+                  "<div><strong>Unique "+scope.panel.labelYaxis +"</strong> <span style='color:red'>" + this.__data__.top_field2.numBuckets + "</span></div>"+
+                  "<div><strong>"+scope.panel.labelYaxis+"</strong> <span style='color:red'>" + this.__data__.top_field2.allBuckets.count + "</span></div>"+
                   "<hr>"+
                   "<h4>Total</h4>"+
                   "<div><strong>"+scope.panel.labelXaxis+"</strong> <span style='color:red'>" + scope.data.field1stat.field_count + "</span></div>"+
-                  "<div><strong>"+scope.panel.labelYaxis+"</strong> <span style='color:red'>" + scope.data.field1stat.tot_docs + "</span></div>";
+                  "<div><strong>"+scope.panel.docname+"</strong> <span style='color:red'>" + scope.data.field1stat.tot_docs + "</span></div>";
               });
 
           var tipField2 = d3tip()
@@ -372,11 +373,11 @@ function (angular, app, _, $, d3, d3tip,palette) {
               .html(function(d) {
                   var tipField2;
                   if(scope.panel.number_mode==="F"){
-                    tipField2="<div><strong>"+scope.panel.labelYaxis+"</strong> <span style='color:red'>" + d.count + "</span></div>";
+                    tipField2="<div><strong>"+scope.panel.docname+"</strong> <span style='color:red'>" + d.count + "</span></div>";
                   }else{
-                    tipField2="<div><strong>"+scope.panel.labelYaxis+" percentage</strong> <span style='color:red'>" + ((d.count/this.parentNode.__data__.top_field2.allBuckets.count)*100).toFixed(2) + "%</span></div>";
+                    tipField2="<div><strong>"+scope.panel.docname+" percentage</strong> <span style='color:red'>" + ((d.count/this.parentNode.__data__.top_field2.allBuckets.count)*100).toFixed(2) + "%</span></div>";
                   }
-                  return "<div><strong>"+scope.panel.labelYaxis+" category name:</strong> <span style='color:red'>" + d.val + "</span></div>"+
+                  return "<div><strong>"+scope.panel.labelYaxis+":</strong> <span style='color:red'>" + d.val + "</span></div>"+
                   tipField2;
               });
 
@@ -497,7 +498,7 @@ function (angular, app, _, $, d3, d3tip,palette) {
               .attr("fill", "#000")
               .attr("font-weight", "bold")
               .attr("text-anchor", "start")
-              .text(scope.panel.labelYaxis);
+              .text(scope.panel.docname);
           };
 
           draw();
