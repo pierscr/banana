c7dbc87f1563a4df4a41d360f8357962951dff2c
pierscr <pierscr@libero.it>
2018-10-03 13:02:21 +0200

    network graph added feature parametric node size

--- src/app/panels/networkgraph/module.js
+++ src/app/panels/networkgraph/module.js
@@ -55,7 +55,10 @@ function (angular, app, _, $, d3,d3tip,dataGraphMapping) {
       charge:-300,
       gravity:0.1,
       maxLinkDistance:100,
-      minLinkDistance:10
+      minLinkDistance:10,
+      minNodeSize:80,
+      maxNodeSize:170,
+      fontSize:15
     };
 
     // Set panel's default values
@@ -206,17 +209,34 @@ function (angular, app, _, $, d3,d3tip,dataGraphMapping) {
 
 
           var zoom = d3.behavior.zoom();
+          var zoomEnable=true;
+          var zoomScale;
+          var zoomtX;
 
           var chart = d3.select(element[0]).append('svg')
                         .attr('width', parent_width)
                         .attr('height', parentheight)
                         .call(zoom);
 
+          chart
+            .on('mouseup',function(){
+                      !zoomEnable && zoom.scale(zoomScale);
+                      !zoomEnable && zoom.translate(zoomtX);
+                      zoomEnable=true;
+                  });
+
           var tipLink = d3tip()
               .attr('class', 'd3-tip')
               .offset([-10, 0])
               .html(function(d) {
                   return "<div><strong>Similarity</strong> <span style='color:red'>" + d.Similarity + "</span></div>";
+              });
+
+          var tipNode = d3tip()
+              .attr('class', 'd3-tip')
+              .offset([-10, 0])
+              .html(function(d) {
+                  return "<div><strong>Frequency</strong> <span style='color:red'>" + d.count + "</span></div>";
               });
 
           // node distance scale
@@ -235,7 +255,16 @@ function (angular, app, _, $, d3,d3tip,dataGraphMapping) {
                                     }),d3.max(scope.data.links,function(link){
                                         return link.Similarity;
                                     })])
-                            .rangeRound([1,3]);
+                            .rangeRound([3,8]);
+
+                            // node distance scale
+          var nodeSize =  d3.scale.linear()
+                            .domain([d3.min(scope.data.nodes,function(node){
+                                        return node.count;
+                                    }),d3.max(scope.data.nodes,function(node){
+                                        return node.count;
+                                    })])
+                          .rangeRound([width/scope.panel.maxNodeSize,width/scope.panel.minNodeSize]);
 
 
           var force = d3.layout
@@ -259,16 +288,17 @@ function (angular, app, _, $, d3,d3tip,dataGraphMapping) {
               .attr('y1', function(d) { return d.source.y; })
               .attr('x2', function(d) { return d.target.x; })
               .attr('y2', function(d) { return d.target.y; })
-              .attr("stroke-width", function(link){return lineStroke(link.Similarity);});
+              .attr("stroke-width", function(link){return lineStroke(link.Similarity);})
+              .on('mouseover', tipLink.show)
+              .on('mouseout', tipLink.hide);
 
-            // link.insert('line')
-            //   .attr('x1', function() { return this.parentNode.__data__.source.x; })
-            //   .attr('y1', function() { return this.parentNode.__data__.source.y; })
-            //   .attr('x2', function() { return this.parentNode.__data__.target.x; })
-            //   .attr('y2', function() { return this.parentNode.__data__.target.y; })
-            //   .attr("stroke-width", 5)
-            //   .on('mouseover', tipLink.show)
-            //   .on('mouseout', tipLink.hide);
+            link.insert('line')
+              .attr('class', 'linkForTips')
+              .attr('x1', function() { return this.parentNode.__data__.source.x; })
+              .attr('y1', function() { return this.parentNode.__data__.source.y; })
+              .attr('x2', function() { return this.parentNode.__data__.target.x; })
+              .attr('y2', function() { return this.parentNode.__data__.target.y; })
+              .attr("stroke-width", 5);
 
           // Now it's the nodes turn. Each node is drawn as a circle.
 
@@ -279,35 +309,59 @@ function (angular, app, _, $, d3,d3tip,dataGraphMapping) {
               .attr("transform", function(d){
                 return "translate("+d.x+","+d.y+")";
               })
-              .on('click', function(d){  tipLink.hide; scope.build_search(scope.panel.nodesField,d.name);});
+              .on('click', function(d){  tipLink.hide; scope.build_search(scope.panel.nodesField,d.name);})
+              .on('mousedown',function(){
+                  zoomScale=zoom.scale();
+                  zoomtX=zoom.translate();
+                  zoomEnable=false;
+              })
+              .on('mouseup',function(){
+                  !zoomEnable && zoom.scale(zoomScale);
+                  !zoomEnable && zoom.translate(zoomtX);
+                  zoomEnable=true;
+              })
+              .on('mouseover', tipNode.show)
+              .on('mouseout', tipNode.hide);
+
 
 
 
 
 
             node.append('circle')
-                .attr('r', width/150)
+                .attr('r', function(d){
+                  return nodeSize(d.count);
+                })
                 .call(force.drag);
 
               node
                   .append('text')
                   .text(function(d){return d.name;})
                   .attr('x',20)
-                  .attr('y',-10);
+                  .attr('y',-10)
+                  .style('font-size',scope.panel.fontSize+'px');
 
             chart.call(tipLink);
+            chart.call(tipNode);
 
               force.on("tick", function() {
 
 
                 node.transition().ease('linear').duration(animationStep)
-                    .attr("transform",function(d){
-                                        return "translate("+d.x*zoom.scale()+","+d.y*zoom.scale()+")translate("+zoom.translate()[0]+","+zoom.translate()[1]+")";
-                                      });
+                                      .attr("transform",function(d){
+                                                          if(zoomEnable){
+                                                            return "translate("+d.x*zoom.scale()+","+d.y*zoom.scale()+")translate("+zoom.translate()[0]+","+zoom.translate()[1]+")";
+                                                          }else{
+                                                            return "translate("+d.x*zoomScale+","+d.y*zoomScale+")translate("+zoomtX[0]+","+zoomtX[1]+")";
+                                                          }
+                                                        });
 
-                node.selectAll('circle')
-                    .transition().ease('linear').duration(animationStep)
-                    .attr('transform','scale('+zoom.scale()+')');
+
+                var animationNode=node.selectAll('circle')
+                    .transition().ease('linear').duration(animationStep);
+
+                zoomEnable && animationNode.attr('transform','scale('+zoom.scale()+')');
+                !zoomEnable && animationNode.attr('transform','scale('+zoomScale+')');
 
 
                 node.selectAll('text')
@@ -325,12 +379,15 @@ function (angular, app, _, $, d3,d3tip,dataGraphMapping) {
               // one iteration, the indices have been replaced by
               // references to the node objects.
 
-              link.transition().ease('linear').duration(animationStep)
+              var animationLink=link.transition().ease('linear').duration(animationStep)
                   .attr('x1', function(d) { return d.source.x; })
                   .attr('y1', function(d) { return d.source.y; })
                   .attr('x2', function(d) { return d.target.x; })
-                  .attr('y2', function(d) { return d.target.y; })
-                  .attr('transform','translate('+zoom.translate()+')scale('+zoom.scale()+')');
+                  .attr('y2', function(d) { return d.target.y; });
+
+
+              zoomEnable && animationLink.attr('transform','translate('+zoom.translate()+')scale('+zoom.scale()+')');
+              !zoomEnable && animationLink.attr('transform','translate('+zoomtX+')scale('+zoomScale+')');
 
 
 
