c082e142fdbfac5257b1a5b29204a093ca241ebe
pierscr <pierscr@libero.it>
2019-08-30 17:40:47 +0200

    collective intelligence integration and parameters check added

--- src/app/panels/sendMessage/module.js
+++ src/app/panels/sendMessage/module.js
@@ -28,31 +28,67 @@ function (angular, app, _, require) {
     };
     _.defaults($scope.panel, _d);
 
-
-    $scope.sendMessage = function(title,description) {
-      var fwdParameters=$location.search();
-      var data=fwdParameters;
-      data.title=title;
-      data.description=description;
-      //filterSrv.removeAll();
-      $http.post($scope.panel.url, data).then(function successCallback(response) {
-          if(response==undefined){
-            $scope.panel.response="Send data error ";
-          }else{
-            if(response.status=200){
-              console.log("send message done");
-              $scope.panel.response="Data sent";
-            }else{
-              $scope.panel.response="Send data error ";
-            }
-          }
-        }, function errorCallback(response) {
-          console.log("error");
-          $scope.response="Send data error ";
-        });
-      $scope.response="Send data error ";
-      dashboard.refresh();
+    function checkParameter(tot,expectedItem){
+      var fwdParameters =  $location.search();
+      for(var item in fwdParameters){
+        if(expectedItem == item)
+          return tot;
+      }
+      tot.push(expectedItem);
+      return tot
     }
+
+    function addParameter(tot,expectedItem){
+      var fwdParameters =  $location.search();
+      for(var item in fwdParameters){
+        if(expectedItem == item){
+          tot[item]=fwdParameters[item];
+          return tot;
+        }
+      }
+    }
+
+
+    $scope.sendMessage = function (title, description) {
+      var fwdParameters = $location.search();
+
+      var checkResult=$scope.panel.fields.reduce(checkParameter,[]);
+      if(checkResult.length>0){
+        console.log('the follow parameters need to be passed with the request');
+        console.log(checkResult);
+        $scope.panel.response = 'You need to reopen the visualization link in the collective intelligence';
+        return;
+      }
+      var data = $scope.panel.fields.reduce(addParameter,{});;
+      data.title = title;
+      data.description = description;
+      var 	wrappedJson={"/pythia-challenge62-portlet.clsidea/addIdea":{"ideaParameters":[]}};
+      wrappedJson["/pythia-challenge62-portlet.clsidea/addIdea"]["ideaParameters"].push(JSON.stringify(data));
+      //filterSrv.removeAll();
+      $http.post($scope.panel.url, wrappedJson).then(function successCallback(response) {
+        if (response == undefined) {
+          $scope.panel.response = 'Send data error ';
+        } else {
+          if (response.status == 200) {
+            if(response.data.success == true){
+              console.log('send message done');
+              $scope.panel.response = 'Data sent';
+            }else{
+              console.log('send message error');
+            $scope.panel.response = response.data.message!=""?response.data.message:'Send data error ';
+            }
+          } else {
+            console.log('send message error');
+            $scope.panel.response = 'Send data error ';
+          }
+        }
+      }, function errorCallback(response) {
+        console.log('error');
+        $scope.response = 'Send data error ';
+      });
+      $scope.response = 'Send data error ';
+      dashboard.refresh();
+    };
 
       $scope.add_facet_field = function(field) {
         if ( _.indexOf($scope.panel.fields, field) === -1 && $scope.panel.fields.length < $scope.panel.maxnum_facets) {
@@ -62,14 +98,14 @@ function (angular, app, _, require) {
 
       $scope.remove_facet_field = function(field) {
         if ( _.indexOf($scope.panel.fields, field) > -1) {
-          $scope.panel.fields = _.without($scope.fields, field);
+          $scope.panel.fields = _.without($scope.panel.fields, field);
         }
       };
 
       $scope.init= function(){
         $scope.panel.response="";
         $scope.panel.messagetitle="";
-        $scope.panel.description="";        
+        $scope.panel.description="";
     };
   });
 });
